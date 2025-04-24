5c0da671c237d394958016ee58180eddac8bfb24
Fatemeh Rajabzade <ra.fatemeh851@gmail.com>
2025-04-24 09:10:22 -0700

    Creat abstract Deive class

---
+++ src/Device.java
@@ -0,0 +1,30 @@
+public abstract class Device {
+    protected String name;
+    protected String protocol;
+    protected String status;
+
+    public Device(String name, String protocol){
+        this.name = name;
+        this.protocol = protocol;
+        this.status = "off";
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getProtocol(){
+        return protocol;
+    }
+
+    public String getStatus(){
+        return status;
+    }
+
+    public void setStatus(String status){
+        this.status = status;
+    }
+
+    //  متد اطلاعات گرفتن از دستگاه ها
+    public abstract String getInfo();
+}
