fcf1bb44d7bbdb043de9705ee4dc75ac37229834
Fatemeh Rajabzade <ra.fatemeh851@gmail.com>
2025-04-24 12:08:12 -0700

    Add rules for devices

--- src/Rule.java
+++ src/Rule.java
@@ -1,4 +1,28 @@
-package PACKAGE_NAME;
-
 public class Rule {
+    private String deviceName;
+    private String time;
+    private String action;
+
+    public Rule(String deviceName, String time, String action){
+        this.deviceName = deviceName;
+        this.time = time;
+        this.action = action;
+    }
+
+    //getter
+    public String getDeviceName(){
+        return deviceName;
+    }
+
+    public String getTime(){
+        return time;
+    }
+
+    public String getAction(){
+        return action;
+    }
+
+    public String getRuleInfo(){
+        return deviceName + " " + time + action;
+    }
 }
